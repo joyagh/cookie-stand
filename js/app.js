d0f2eabb9c7ef0656d02e20f17fe4ab348a1c0ac
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-04-29 16:28:18 -0500

    milestone

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let stores = [
+     { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3},
+     { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2},
+     { store: "Dubai", minCust: 11, maxCust: 38, avgCookiesPerSale: 3.7},
+     { store: "Paris", minCust: 20, maxCust: 38, avgCookiesPerSale: 2.3},
+     { store: "Lima", minCust: 2, maxCust: 16, avgCookiesPerSale: 4.6},
+]
+
+let CSaSea = [21, 5, 7, 12]
+
+let seaSales1 = document.getElementById("sea-sales-1"); 
+seaSales1.textContent = "21"
+let seaSales2 = document.getElementById("sea-sales-2"); 
+seaSales2.textContent = "5"
+let seaSales3 = document.getElementById("sea-sales-3"); 
+seaSales3.textContent = "7"
+let seaSales4 = document.getElementById("sea-sales-4"); 
+seaSales4.textContent = "12"
