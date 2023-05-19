7ddeeb8b4ebbb84d03eb1acabf4e2eb273a202dd
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-05-18 19:09:07 -0500

    js updates

--- js/app.js
+++ js/app.js
@@ -1,21 +1,62 @@
 "use strict";
 
 let stores = [
-  { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3 },
-  { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2 },
-  { store: "Dubai", minCust: 11, maxCust: 38, avgCookiesPerSale: 3.7 },
-  { store: "Paris", minCust: 20, maxCust: 38, avgCookiesPerSale: 2.3 },
-  { store: "Lima", minCust: 2, maxCust: 16, avgCookiesPerSale: 4.6 },
+  {
+    store: "Seattle",
+    minCust: 23,
+    maxCust: 65,
+    avgCookiesPerSale: 6.3,
+    salesPerHour: [],
+  },
+  {
+    store: "Tokyo",
+    minCust: 3,
+    maxCust: 24,
+    avgCookiesPerSale: 1.2,
+    salesPerHour: [],
+  },
+  {
+    store: "Dubai",
+    minCust: 11,
+    maxCust: 38,
+    avgCookiesPerSale: 3.7,
+    salesPerHour: [],
+  },
+  {
+    store: "Paris",
+    minCust: 20,
+    maxCust: 38,
+    avgCookiesPerSale: 2.3,
+    salesPerHour: [],
+  },
+  {
+    store: "Lima",
+    minCust: 2,
+    maxCust: 16,
+    avgCookiesPerSale: 4.6,
+    salesPerHour: [],
+  },
 ];
 
-let CSaSea = [25, 55, 8, 12];
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
 
-let seaSales1 = document.getElementById("sea-sales-1");
-seaSales1.textContent = CSaSea[0];
-let seaSales2 = document.getElementById("sea-sales-2");
-seaSales2.textContent = CSaSea[1];
-let seaSales3 = document.getElementById("sea-sales-3");
-seaSales3.textContent = CSaSea[2];
-let seaSales4 = document.getElementById("sea-sales-4");
-seaSales4.textContent = CSaSea[3];
+let hours = [`11am`, `12pm`, `1pm`, `2pm`];
+
+for (let i = 0; i < stores.length; i++) {
+  for (let j = 0; j < hours.length; j++) {
+    let randomNum = getRandomInt(stores[i].minCust, stores[i].maxCust);
+    stores[i].salesPerHour.push(randomNum);
+  }
+}
+console.log(stores)
+
+let seattleSales = document.querySelectorAll("#seaSa1");
+for (let i = 0; i < seattleSales.length; i++){
+  console.log(hours[0], stores[0]);
+}
 
